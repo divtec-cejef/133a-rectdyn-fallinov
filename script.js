4d06e61f13b870d4ae549f81eeff9e7382a10568
Steve Fallet <steve.fallet@gmail.com>
2023-10-03 13:45:32 +0200

    demo des bases

--- script.js
+++ script.js
@@ -7,4 +7,32 @@
  */
 
 'use strict'; // Demande un interprétation stricte du code
-console.log("Coucou !");
+
+// Récupération des éléments HTML utiles
+const formRect = document.getElementById('formRect');
+const txtHauteur = document.getElementById('hauteur');
+const divRectangle = document.getElementById('rectangle');
+console.log(formRect, txtHauteur, divRectangle);
+
+/**
+ * Fonction appelée lors de l'envoi du formulaire
+ */
+function miseAJour(event) {
+  // Stoppe l'envoi du formulaire
+  event.preventDefault();
+  let hauteur = parseInt(txtHauteur.value); // Retourne un entier ou NaN
+  if(isNaN(hauteur)) {
+    alert('Entrez un nombre !');
+    return; // Stoppe le script, code de la fonction
+  }
+  divRectangle.style.height = hauteur + 'px';
+}
+
+// Ecoute l'envoi du formulaire
+formRect.addEventListener('submit', miseAJour);
+
+// formRect.addEventListener('submit', () => {
+//   event.preventDefault();
+//   alert('miseAJour');
+// });
+
